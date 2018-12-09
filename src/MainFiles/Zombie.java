2c3be73f4694561b3480379654c7a64bc6592653
Ajver <zawlockidominik@gmail.com>
2018-12-09 20:11:48 +0100

    Comments + zombie hit timer changed

--- src/MainFiles/Zombie.java
+++ src/MainFiles/Zombie.java
@@ -9,8 +9,14 @@ import java.util.Random;
 
 public class Zombie extends jslObject {
 
+    // How many zombies are in this level
     private static int zombiesNr = 0;
+
+    // Max zombies on map in the same place
     private static int maxZombies = 20;
+
+    // Waiting area
+    // Zombies to spawn (created once, then adding into jslManager)
     private static LinkedList<Zombie> zombies = new LinkedList<>();
 
     private BufferedImage texture = Texture.zombieImg;
@@ -20,12 +26,13 @@ public class Zombie extends jslObject {
     private CollisionBox collisionBox;
     private ZombieHP hp;
 
-    private float vel;
+    // Velocity of moving
+    private float vel = 100.0f;
 
-    private float strange;
+    private float strange = 10.0f;
     private float hitRate = 1.0f;
-    private float timer = 1.0f / hitRate;
-    private boolean isReady = false;
+    private jslTimer hitTimer;
+    private boolean isReady;
 
     private jslTimer soundTimer;
     private jslSound[] zombieSounds = new jslSound[4];
@@ -38,25 +45,29 @@ public class Zombie extends jslObject {
         this.setRotateToCenter();
         this.setLabel(jslLabel.ZOMBIE);
         this.jsl = jsl;
-        this.strange = 10.0f;
-        this.vel = 100.0f;
 
         soundTimer = new jslTimer((r.nextInt(4) + 3));
         collisionBox = new CollisionBox(this) {
             @Override
             public void onCollision(jslObject other) {
                 if (other.is(jslLabel.BULLET)) {
+                    // Vector to be punched
                     jslVector2 v = new jslVector2(other.getVelX(), other.getVelY());
                     v.normalize();
                     v.multiply(16);
                     move(v.x, v.y);
+
+                    // Remove bullet
                     jsl.removeObject(other);
                     if (!hp.addHp(-13 - r.nextInt(20))) {
                         zombiesNr--;
+
                         Stain stain = new Stain(o.getX(), o.getY(), o.getW(), o.getH());
                         stain.setRotate(o.getRotate());
-                        jsl.add(stain);
+
+                        // Back zombie to waiting area
                         zombies.add((Zombie)o);
+                        jsl.add(stain);
                         jsl.removeObject(hp);
                         jsl.removeObject(o);
                     }
@@ -78,10 +89,14 @@ public class Zombie extends jslObject {
             zombieSounds[i].setLevel(0.6f);
         }
 
+        hitTimer = new jslTimer(1.0f / hitRate);
+
         reset(0, 0);
     }
 
     public void reset(float x, float y) {
+        isReady = false;
+        hitTimer.restart();
         setPosition(x, y);
         jsl.add(this.hp = new ZombieHP(50, getW(),getW()*1.5f, 10));
         soundTimer.restart();
@@ -95,6 +110,7 @@ public class Zombie extends jslObject {
             v.multiply(vel);
             move(v.x * et, v.y * et);
 
+            // Rotate to player
             float theta = (float) Math.atan2(v.x, v.y);
             setRotate(2 * (float) Math.PI - theta);
         }
@@ -104,25 +120,26 @@ public class Zombie extends jslObject {
             zombieSounds[r.nextInt(zombieSounds.length)].play();
         }
 
-        // Collision
         collisionBox.collision(jsl);
 
         hp.setPosition(getX(), getY());
 
         if(!isReady) {
-            timer -= et;
-
-            if(timer <= 0) {
-                timer = 1.0f / hitRate;
+            if(hitTimer.update()) {
                 isReady = true;
             }
         }else {
+            // If player is close enough...
             float dx = getCenterX() - player.getCenterX();
             float dy = getCenterY() - player.getCenterY();
 
             if (Math.abs(dx) <= (player.getW() + getW()) * 0.6f) {
                 if (Math.abs(dy) <= (player.getH() + getH()) * 0.5f) {
+                    // ...hit him
                     HUD.addHp(-strange);
+
+                    // Restart hit timer
+                    hitTimer.restart();
                     isReady = false;
                 }
             }
@@ -138,11 +155,12 @@ public class Zombie extends jslObject {
     }
 
     public static boolean newZombie(jslManager jsl, float x, float y) {
+        // All zombies are spawned
         if(zombies.isEmpty()) {
             return false;
         }
 
-        // If all zombies, that should be spawned are spawned, do not create next
+        // If all zombies, that should be spawned (in this level) are spawned, do not create next
         if(zombies.size() + zombiesNr == maxZombies) {
             return false;
         }
@@ -155,6 +173,7 @@ public class Zombie extends jslObject {
     }
 
     public static void fillZombies(float w, float h, jslManager jsl) {
+        // Fill waiting area
         for(int i=0; i<maxZombies; i++) {
             zombies.add(new Zombie(w, h, jsl));
         }
